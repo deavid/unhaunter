8ff0eacca107dad68936c672d4c83e9f5a46d73e
David Martinez Marti <deavidsedice@gmail.com>
2023-12-18 09:56:06 +0000

    updates to assets and maps

---
+++ assets/maps/unhaunter_custom_tileset.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="unhaunter_custom_tileset" tilewidth="162" tileheight="132" tilecount="1" columns="0" objectalignment="bottom">
+ <grid orientation="isometric" width="72" height="36"/>
+ <tile id="0">
+  <image width="162" height="132" source="../img/van.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="3" x="-46" y="-60" width="108" height="71"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/maps/unhaunter_spritesheetA_3x3x3.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="unhaunter_spritesheetA_3x3x3" tilewidth="32" tileheight="32" tilecount="264" columns="12">
+ <tileoffset x="-4" y="0"/>
+ <grid orientation="isometric" width="32" height="32"/>
+ <image source="../img/spritesheetA_3x3x3.png" width="384" height="704"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="stones" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="grass" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="asphalt" color="#0000ff" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="2" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="3" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="4" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="5" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="6" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="7" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="8" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="9" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="10" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="11" wangid="0,3,0,3,0,3,0,3"/>
+  </wangset>
+  <wangset name="Unnamed Set" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
+</tileset>
