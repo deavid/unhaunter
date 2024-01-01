b6dcbd4f569efb3d0bd2bc1cba181a826ddc4b22
David Martinez Marti <deavidsedice@gmail.com>
2024-01-01 08:25:25 +0000

    Bevy map load - lacks horizontal flips.

--- src/testmapload.rs
+++ src/testmapload.rs
@@ -1,11 +1,13 @@
 use std::{
     collections::HashMap,
     fmt::{Debug, Display},
+    slice::Iter,
+    sync::Arc,
 };
 
 use bevy::{ecs::system::adapter::dbg, sprite::Anchor};
 use serde::de;
-use tiled::{Loader, PropertyValue, TileLayer};
+use tiled::{Loader, PropertyValue, TileLayer, Tileset};
 
 #[derive(Clone, Copy)]
 struct Pos<T: Clone + Copy + Debug> {
@@ -42,6 +44,8 @@ impl Debug for MapTile {
 #[derive(Debug, Clone)]
 struct MapLayer {
     name: String,
+    visible: bool,
+    opacity: f32,
     offset: Pos<f32>,
     user_class: Option<String>,
     user_properties: HashMap<String, tiled::PropertyValue>,
@@ -52,9 +56,58 @@ struct MapLayerGroup {
     layers: Vec<MapLayer>,
 }
 
+impl MapLayerGroup {
+    fn iter(&self) -> IterMapLayerGroup {
+        IterMapLayerGroup {
+            iter: vec![self.layers.iter()],
+        }
+    }
+}
+
+struct IterMapLayerGroup<'a> {
+    iter: Vec<Iter<'a, MapLayer>>,
+}
+
+impl<'a> Iterator for IterMapLayerGroup<'a> {
+    type Item = &'a MapLayer;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let op_iter = self.iter.pop();
+        if let Some(mut iter) = op_iter {
+            if let Some(layer) = iter.next() {
+                self.iter.push(iter);
+                match &layer.data {
+                    MapLayerType::Tiles(_) => Some(layer),
+                    MapLayerType::Objects() => self.next(),
+                    MapLayerType::Image() => self.next(),
+                    MapLayerType::Group(g) => {
+                        self.iter.push(g.layers.iter());
+                        self.next()
+                    }
+                }
+            } else {
+                self.next()
+            }
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Clone)]
+struct MapTileList {
+    v: Vec<MapTile>,
+}
+
+impl Debug for MapTileList {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{:?}", &self.v))
+    }
+}
+
 #[derive(Debug, Clone)]
 enum MapLayerType {
-    Tiles(Vec<MapTile>),
+    Tiles(MapTileList),
     Objects(),
     Image(),
     Group(MapLayerGroup),
@@ -101,9 +154,11 @@ fn load_tile_layer_iter<'a>(
     for layer in layer_iter {
         let map_layer = MapLayer {
             name: layer.name.to_string(),
+            visible: layer.visible,
             offset: Pos::new(layer.offset_x, layer.offset_y),
             user_class: layer.user_type.clone(),
             user_properties: layer.properties.clone(),
+            opacity: layer.opacity,
             data: load_tile_layer(layer),
         };
 
@@ -128,7 +183,7 @@ fn load_tile_group_layer(layer: tiled::GroupLayer) -> MapLayerGroup {
     MapLayerGroup { layers }
 }
 
-fn load_tile_layer_tiles(layer: tiled::TileLayer) -> Vec<MapTile> {
+fn load_tile_layer_tiles(layer: tiled::TileLayer) -> MapTileList {
     let mut ret = vec![];
 
     for y in 0..layer.height().unwrap() as i32 {
@@ -145,8 +200,7 @@ fn load_tile_layer_tiles(layer: tiled::TileLayer) -> Vec<MapTile> {
             }
         }
     }
-    eprintln!("{} tiles loaded", ret.len());
-    ret
+    MapTileList { v: ret }
 }
 
 use bevy::prelude::*;
@@ -159,16 +213,30 @@ fn main() {
             // by linear filtering.
             ImagePlugin::default_nearest(),
         ))
+        .init_resource::<MapTileSetDb>()
         .add_systems(Startup, setup)
         //        .add_systems(Update, sprite_movement)
         .add_systems(Update, camera_movement)
         .run();
 }
 
+#[derive(Debug, Clone)]
+struct MapTileSet {
+    tileset: Arc<Tileset>,
+    handle: Handle<TextureAtlas>,
+    y_anchor: f32,
+}
+
+#[derive(Debug, Clone, Default, Resource)]
+struct MapTileSetDb {
+    db: HashMap<String, MapTileSet>,
+}
+
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    mut tilesetdb: ResMut<MapTileSetDb>,
 ) {
     commands.spawn(Camera2dBundle::default());
 
@@ -187,6 +255,7 @@ fn setup(
         // if &tileset.name != "A6x6x10" {
         //     continue;
         // }
+
         if let Some(image) = &tileset.image {
             let img_src = image
                 .source
@@ -233,52 +302,96 @@ fn setup(
             };
             dbg!(y_anchor);
             let atlas1_handle = texture_atlases.add(atlas1);
-            for (id, _tile) in tileset.tiles() {
-                let mut id2 = TextureAtlasSprite::new(id as usize);
-                let x = (id % tileset.columns) as f32 * tileset.tile_width as f32 * 2.1
-                    - n as f32 * 900.0
-                    - 100.0;
-                let y = 300.0 - (id / tileset.columns) as f32 * tileset.tile_height as f32 * 2.1;
-                // let old_anchor = id2.anchor.as_vec();
-                // id2.anchor = Anchor::Custom(Vec2::new(old_anchor.x, old_anchor.y * 1.5));
-                id2.anchor = Anchor::Custom(Vec2::new(0.0, y_anchor));
-                commands.spawn(SpriteSheetBundle {
-                    texture_atlas: atlas1_handle.clone(),
-                    sprite: id2,
-                    transform: Transform {
-                        translation: Vec3::new(x, y, 0.0),
+            let mts = MapTileSet {
+                tileset: tileset.clone(),
+                handle: atlas1_handle.clone(),
+                y_anchor,
+            };
+            if tilesetdb.db.insert(tileset.name.to_string(), mts).is_some() {
+                eprintln!("ERROR: Already existing tileset loaded with name {:?} - make sure you don't have the same tileset loaded twice", tileset.name.to_string());
+                panic!();
+            }
+            const DEBUG_SPRITE_ANCHOR: bool = false;
+            if DEBUG_SPRITE_ANCHOR {
+                for (id, _tile) in tileset.tiles() {
+                    let mut id2 = TextureAtlasSprite::new(id as usize);
+                    let x = (id % tileset.columns) as f32 * tileset.tile_width as f32 * 2.1
+                        - n as f32 * 900.0
+                        - 100.0;
+                    let y =
+                        300.0 - (id / tileset.columns) as f32 * tileset.tile_height as f32 * 2.1;
+                    // let old_anchor = id2.anchor.as_vec();
+                    // id2.anchor = Anchor::Custom(Vec2::new(old_anchor.x, old_anchor.y * 1.5));
+                    id2.anchor = Anchor::Custom(Vec2::new(0.0, y_anchor));
+                    commands.spawn(SpriteSheetBundle {
+                        texture_atlas: atlas1_handle.clone(),
+                        sprite: id2,
+                        transform: Transform {
+                            translation: Vec3::new(x, y, 0.0),
+                            ..default()
+                        },
                         ..default()
-                    },
-                    ..default()
-                });
-                // Rectangle
-                commands.spawn(SpriteBundle {
-                    sprite: Sprite {
-                        color: Color::rgba(1.0, 0.1, 0.0, 0.1),
-                        custom_size: Some(Vec2::new(4.0, 4.0)),
+                    });
+                    // Rectangle
+                    commands.spawn(SpriteBundle {
+                        sprite: Sprite {
+                            color: Color::rgba(1.0, 0.1, 0.0, 0.1),
+                            custom_size: Some(Vec2::new(4.0, 4.0)),
+                            ..default()
+                        },
+                        transform: Transform::from_translation(Vec3::new(x, y, 0.01)),
                         ..default()
-                    },
-                    transform: Transform::from_translation(Vec3::new(x, y, 0.01)),
-                    ..default()
-                });
-                commands.spawn(SpriteBundle {
-                    sprite: Sprite {
-                        color: Color::rgba(1.0, 0.1, 0.0, 0.2),
-                        custom_size: Some(Vec2::new(2.0, 2.0)),
+                    });
+                    commands.spawn(SpriteBundle {
+                        sprite: Sprite {
+                            color: Color::rgba(1.0, 0.1, 0.0, 0.2),
+                            custom_size: Some(Vec2::new(2.0, 2.0)),
+                            ..default()
+                        },
+                        transform: Transform::from_translation(Vec3::new(x, y, 0.02)),
                         ..default()
-                    },
-                    transform: Transform::from_translation(Vec3::new(x, y, 0.02)),
-                    ..default()
-                });
-                commands.spawn(SpriteBundle {
-                    sprite: Sprite {
-                        color: Color::rgba(1.0, 0.1, 0.0, 0.4),
-                        custom_size: Some(Vec2::new(1.0, 1.0)),
+                    });
+                    commands.spawn(SpriteBundle {
+                        sprite: Sprite {
+                            color: Color::rgba(1.0, 0.1, 0.0, 0.4),
+                            custom_size: Some(Vec2::new(1.0, 1.0)),
+                            ..default()
+                        },
+                        transform: Transform::from_translation(Vec3::new(x, y, 0.03)),
                         ..default()
-                    },
-                    transform: Transform::from_translation(Vec3::new(x, y, 0.03)),
-                    ..default()
-                });
+                    });
+                }
+            }
+        }
+    }
+    // end of tileset load
+
+    let map_layers = load_tile_layer_iter(map.layers());
+    let grp = MapLayerGroup { layers: map_layers };
+    for (n, layer) in grp
+        .iter()
+        .filter(|x| x.visible && x.opacity > 0.9)
+        .enumerate()
+    {
+        let z: f32 = n as f32 / 1000.0;
+        if let MapLayerType::Tiles(tiles) = &layer.data {
+            for tile in &tiles.v {
+                let x = map.tile_width as f32 * (tile.pos.x - tile.pos.y) as f32 / 2.0;
+                let y = map.tile_height as f32 * (-tile.pos.x - tile.pos.y) as f32 / 2.0;
+                let op_tileset = tilesetdb.db.get(&tile.tileset);
+                if let Some(tileset) = op_tileset {
+                    let mut id = TextureAtlasSprite::new(tile.tileuid as usize);
+                    id.anchor = Anchor::Custom(Vec2::new(0.0, tileset.y_anchor));
+                    commands.spawn(SpriteSheetBundle {
+                        texture_atlas: tileset.handle.clone(),
+                        sprite: id,
+                        transform: Transform {
+                            translation: Vec3::new(x, y, z),
+                            ..default()
+                        },
+                        ..default()
+                    });
+                }
             }
         }
     }
