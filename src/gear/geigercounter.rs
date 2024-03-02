2f30d7c2344fd32add273fbb8f7e1cbafd1fadcc
David Martinez Marti <deavidsedice@gmail.com>
2024-03-02 18:54:21 +0000

    Add GeigerCounter to the gear that identifies the ghost

--- src/gear/geigercounter.rs
+++ src/gear/geigercounter.rs
@@ -1,16 +1,32 @@
 use bevy::prelude::*;
+use rand::Rng as _;
 
-use super::{on_off, Gear, GearKind, GearSpriteID, GearUsable};
+use crate::{board::Position, ghost_definitions::Evidence};
 
-#[derive(Component, Debug, Clone, Default, PartialEq, Eq)]
+use super::{on_off, playergear::EquipmentPosition, Gear, GearKind, GearSpriteID, GearUsable};
+
+#[derive(Component, Debug, Clone, Default, PartialEq)]
 pub struct GeigerCounter {
     pub enabled: bool,
+    pub display_secs_since_last_update: f32,
+    pub frame_counter: u16,
+    pub sound_a1: f32,
+    pub sound_a2: f32,
+    pub sound_display: f32,
+    pub sound_l: Vec<f32>,
+    pub last_sound_time_secs: f32,
 }
 
 impl GearUsable for GeigerCounter {
     fn get_sprite_idx(&self) -> GearSpriteID {
         match self.enabled {
-            true => GearSpriteID::GeigerOn,
+            true => {
+                if self.sound_a1 > 10.0 {
+                    GearSpriteID::GeigerTick
+                } else {
+                    GearSpriteID::GeigerOn
+                }
+            }
             false => GearSpriteID::GeigerOff,
         }
     }
@@ -23,7 +39,7 @@ impl GearUsable for GeigerCounter {
         let name = self.get_display_name();
         let on_s = on_off(self.enabled);
         let msg = if self.enabled {
-            "Clicks: 15/s".to_string()
+            format!("Reading: {:.1}cpm", self.sound_display)
         } else {
             "".to_string()
         };
@@ -34,6 +50,51 @@ impl GearUsable for GeigerCounter {
         self.enabled = !self.enabled;
     }
 
+    fn update(&mut self, gs: &mut super::GearStuff, pos: &Position, _ep: &EquipmentPosition) {
+        let mut rng = rand::thread_rng();
+        self.display_secs_since_last_update += gs.time.delta_seconds();
+        self.frame_counter += 1;
+        self.frame_counter %= 65413;
+        const K: f32 = 0.5;
+        let pos = Position {
+            x: pos.x + rng.gen_range(-K..K) + rng.gen_range(-K..K),
+            y: pos.y + rng.gen_range(-K..K) + rng.gen_range(-K..K),
+            z: pos.z + rng.gen_range(-K..K) + rng.gen_range(-K..K),
+            global_z: pos.global_z,
+        };
+        let bpos = pos.to_board_position();
+        for (i, bpos) in bpos.xy_neighbors(3).iter().enumerate() {
+            let sound = gs.bf.sound_field.get(bpos).cloned().unwrap_or_default();
+            let sound_reading = sound.iter().sum::<Vec2>().length() * 1000.0;
+            if self.sound_l.len() < 30000 {
+                self.sound_l.push(sound_reading);
+            }
+            let n = (self.frame_counter as usize + i) % self.sound_l.len();
+            self.sound_l[n] += sound_reading * 10.0;
+        }
+        let sum_snd: f32 = self.sound_l.iter().sum();
+        let avg_snd: f32 = sum_snd / self.sound_l.len() as f32;
+        let avg_snd = if gs.bf.evidences.contains(&Evidence::CPM500) {
+            f32::tanh(avg_snd / 300.0) * 600.0
+        } else {
+            f32::tanh(avg_snd / 200.0) * 450.0
+        };
+        const MASS: f32 = 16.0;
+        self.sound_a1 = (self.sound_a1 * MASS + avg_snd * MASS.recip()) / (MASS + MASS.recip());
+        self.sound_a2 =
+            (self.sound_a2 * MASS + self.sound_a1 * MASS.recip()) / (MASS + MASS.recip());
+        self.sound_l.iter_mut().for_each(|x| *x /= 1.0004);
+
+        if gs.time.elapsed_seconds() - self.last_sound_time_secs > 60.0 / avg_snd && self.enabled {
+            self.last_sound_time_secs = gs.time.elapsed_seconds() + rng.gen_range(0.01..0.02);
+            gs.play_audio("sounds/effects-chirp-click.ogg".into(), 0.25);
+        }
+
+        if self.display_secs_since_last_update > 0.1 {
+            self.display_secs_since_last_update = 0.0;
+            self.sound_display = self.sound_a2;
+        }
+    }
     fn box_clone(&self) -> Box<dyn GearUsable> {
         Box::new(self.clone())
     }
