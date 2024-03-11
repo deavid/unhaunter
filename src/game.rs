cf91402ede971ec773f3e5c939b591a7698be322
David Martinez Marti <deavidsedice@gmail.com>
2024-03-11 20:16:06 +0000

    Demonstration for evidence selector

--- src/game.rs
+++ src/game.rs
@@ -75,20 +75,6 @@ pub enum SpriteType {
     Other,
 }
 
-pub fn app_setup(app: &mut App) {
-    app.init_resource::<GameConfig>()
-        .add_event::<RoomChangedEvent>()
-        .add_event::<LoadLevelEvent>()
-        .add_systems(Update, roomchanged_event)
-        .add_systems(OnEnter(root::State::InGame), setup)
-        .add_systems(OnEnter(root::State::InGame), setup_ui)
-        .add_systems(OnExit(root::State::InGame), cleanup)
-        .add_systems(OnEnter(root::GameState::None), resume)
-        .add_systems(OnExit(root::GameState::None), pause)
-        .add_systems(Update, keyboard)
-        .add_systems(PostUpdate, load_level);
-}
-
 pub fn setup(
     mut commands: Commands,
     qc: Query<Entity, With<GCameraArena>>,
@@ -254,12 +240,14 @@ pub fn setup_ui(mut commands: Commands, handles: Res<root::GameAssets>) {
             parent
                 .spawn(NodeBundle {
                     border_color: DEBUG_BCOLOR,
+                    background_color: BackgroundColor(Color::rgba(0.0, 0.0, 0.0, 0.4)),
                     style: Style {
                         border: UiRect::all(Val::Px(1.0)),
                         padding: UiRect::all(Val::Px(1.0)),
                         height: Val::Px(100.0),
                         width: Val::Percent(99.9),
                         flex_direction: FlexDirection::Row,
+                        column_gap: Val::Px(6.0),
                         ..Default::default()
                     },
                     ..Default::default()
@@ -271,10 +259,12 @@ pub fn setup_ui(mut commands: Commands, handles: Res<root::GameAssets>) {
                     parent
                         .spawn(NodeBundle {
                             border_color: DEBUG_BCOLOR,
+                            background_color: BackgroundColor(PANEL_BGCOLOR),
                             style: Style {
                                 border: UiRect::all(Val::Px(1.0)),
-                                padding: UiRect::all(Val::Px(1.0)),
-                                flex_grow: 1.0,
+                                padding: UiRect::all(Val::Px(6.0)),
+                                flex_grow: 0.0,
+                                min_width: Val::Px(200.0),
                                 align_content: AlignContent::Center,
                                 align_items: AlignItems::Center,
                                 ..Default::default()
@@ -284,7 +274,7 @@ pub fn setup_ui(mut commands: Commands, handles: Res<root::GameAssets>) {
                         .with_children(|parent| {
                             // For now a reminder of the keys:
                             let text_bundle = TextBundle::from_section(
-                                "Movement: WASD - Interact: E\nToggle Aux: T - Toggle Main: R\nCycle Inv: Q - Swap: TAB",
+                                "Movement: WASD - Interact: E\nToggle Aux: T - Toggle Main: R\nCycle Inv: Q - Swap: TAB\nChange Evidence: C",
                                 TextStyle {
                                     font: handles.fonts.chakra.w300_light.clone(),
                                     font_size: 18.0,
@@ -298,13 +288,41 @@ pub fn setup_ui(mut commands: Commands, handles: Res<root::GameAssets>) {
                     // Mid side
                     parent.spawn(NodeBundle {
                         border_color: DEBUG_BCOLOR,
+                        background_color: BackgroundColor(PANEL_BGCOLOR),
                         style: Style {
                             border: UiRect::all(Val::Px(1.0)),
-                            padding: UiRect::all(Val::Px(1.0)),
+                            padding: UiRect::all(Val::Px(8.0)),
                             flex_grow: 1.0,
+                            max_width: Val::Percent(60.0),
                             ..Default::default()
                         },
                         ..Default::default()
+                    }).with_children(|parent| {
+                        // For now a reminder of the keys:
+                        let text_bundle = TextBundle::from_sections([
+                            TextSection{value: "Freezing temps:".into(), 
+                                style: TextStyle {
+                                    font: handles.fonts.chakra.w400_regular.clone(),
+                                    font_size: 22.0,
+                                    color: INVENTORY_STATS_COLOR.with_a(1.0),
+                                },
+                            },
+                            TextSection{value: " [+] Evidence Found\n".into(), 
+                                style: TextStyle {
+                                    font: handles.fonts.victormono.w600_semibold.clone(),
+                                    font_size: 20.0,
+                                    color: Color::GREEN.with_a(0.4),
+                                },
+                            },
+                            TextSection{value: "The ghost and the breach will make the ambient colder.\nSome ghosts will make the temperature drop below 0.0ºC.".into(), 
+                                style: TextStyle {
+                                    font: handles.fonts.chakra.w300_light.clone(),
+                                    font_size: 20.0,
+                                    color: INVENTORY_STATS_COLOR,
+                                },
+                            },
+                        ]);
+                        parent.spawn(text_bundle);
                     });
 
                     // Right side
@@ -815,4 +833,18 @@ pub fn roomchanged_event(
 pub enum InteractionExecutionType {
     ChangeState,
     ReadRoomState,
+}
+
+pub fn app_setup(app: &mut App) {
+    app.init_resource::<GameConfig>()
+        .add_event::<RoomChangedEvent>()
+        .add_event::<LoadLevelEvent>()
+        .add_systems(Update, roomchanged_event)
+        .add_systems(OnEnter(root::State::InGame), setup)
+        .add_systems(OnEnter(root::State::InGame), setup_ui)
+        .add_systems(OnExit(root::State::InGame), cleanup)
+        .add_systems(OnEnter(root::GameState::None), resume)
+        .add_systems(OnExit(root::GameState::None), pause)
+        .add_systems(Update, keyboard)
+        .add_systems(PostUpdate, load_level);
 }
