97cb5a094fbc157a3ac398e31c565212c7f565a0
David Martinez Marti <deavidsedice@gmail.com>
2024-03-24 09:50:32 +0000

    Make the Loadout UI fit better

--- src/truck/loadoutui.rs
+++ src/truck/loadoutui.rs
@@ -41,10 +41,17 @@ pub fn setup_loadout_ui(
         background_color: colors::TRUCKUI_ACCENT2_COLOR.into(),
         border_color: colors::TRUCKUI_ACCENT_COLOR.into(),
         style: Style {
+            justify_content: JustifyContent::Center,
+            justify_items: JustifyItems::Center,
+            justify_self: JustifySelf::Center,
+            align_content: AlignContent::Center,
+            align_items: AlignItems::Center,
+            align_self: AlignSelf::Center,
+
             border: UiRect::all(Val::Px(2.0 * UI_SCALE)),
             margin: UiRect::all(Val::Px(3.0 * UI_SCALE)),
-            width: Val::Px(70.0 * UI_SCALE),
-            height: Val::Px(74.0 * UI_SCALE),
+            max_width: Val::Px(70.0 * UI_SCALE),
+            max_height: Val::Px(74.0 * UI_SCALE),
             ..default()
         },
         ..default()
@@ -62,6 +69,7 @@ pub fn setup_loadout_ui(
         style: Style {
             width: Val::Px(64.0 * UI_SCALE),
             height: Val::Px(64.0 * UI_SCALE),
+            margin: UiRect::all(Val::Px(-4.0)),
             ..default()
         },
         ..default()
@@ -81,138 +89,180 @@ pub fn setup_loadout_ui(
         },
         ..default()
     };
-    p.spawn(
-        TextBundle::from_section(
-            "Player Inventory:",
-            TextStyle {
-                font: handles.fonts.chakra.w300_light.clone(),
-                font_size: 25.0 * UI_SCALE,
-                color: colors::TRUCKUI_TEXT_COLOR,
-            },
-        )
-        .with_style(Style {
-            margin: UiRect::all(Val::Px(4.0 * UI_SCALE)),
-            ..default()
-        }),
-    );
-    p.spawn(NodeBundle {
-        style: Style {
-            justify_content: JustifyContent::FlexStart,
-            flex_direction: FlexDirection::Row,
-            flex_grow: 0.04,
-            flex_wrap: FlexWrap::Wrap,
-            ..default()
-        },
-        ..default()
-    })
-    .with_children(|p| {
-        p.spawn(equipment_frame(materials)).with_children(|p| {
-            p.spawn(button())
-                .insert(LoadoutButton::Inventory(playergear::Inventory::new_left()))
-                .with_children(|p| {
-                    p.spawn(equipment_def())
-                        .insert(playergear::Inventory::new_left());
-                });
-        });
-        p.spawn(equipment_frame(materials)).with_children(|p| {
-            p.spawn(button())
-                .insert(LoadoutButton::Inventory(playergear::Inventory::new_right()))
-                .with_children(|p| {
-                    p.spawn(equipment_def())
-                        .insert(playergear::Inventory::new_right());
-                });
-        });
-        p.spawn(equipment_frame(materials)).with_children(|p| {
-            for i in 0..2 {
-                p.spawn(button())
-                    .insert(LoadoutButton::InventoryNext(
-                        playergear::InventoryNext::new(i),
-                    ))
-                    .with_children(|p| {
-                        p.spawn(equipment_def())
-                            .insert(playergear::InventoryNext::new(i));
-                    });
-            }
-        });
-        p.spawn(NodeBundle {
-            style: Style {
-                flex_grow: 1.0,
-                flex_shrink: 1.0,
-                flex_basis: Val::Px(0.0),
-                ..default()
-            },
-            ..default()
-        });
+
+    let left_side = |p: &mut ChildBuilder| {
         p.spawn(
             TextBundle::from_section(
-                "Select ...",
+                "Player Inventory:",
                 TextStyle {
-                    font: handles.fonts.titillium.w400_regular.clone(),
-                    font_size: 22.0 * UI_SCALE,
-                    color: colors::TRUCKUI_TEXT_COLOR.with_a(0.7),
+                    font: handles.fonts.chakra.w300_light.clone(),
+                    font_size: 25.0 * UI_SCALE,
+                    color: colors::TRUCKUI_TEXT_COLOR,
                 },
             )
             .with_style(Style {
                 margin: UiRect::all(Val::Px(4.0 * UI_SCALE)),
-                flex_grow: 0.0,
-                flex_shrink: 0.0,
-                flex_basis: Val::Px(400.0 * UI_SCALE),
-                height: Val::Px(220.0 * UI_SCALE),
-                overflow: Overflow::visible(),
                 ..default()
             }),
-        )
-        .insert(GearHelp);
-    });
-    p.spawn(
-        TextBundle::from_section(
-            "Van Inventory:",
-            TextStyle {
-                font: handles.fonts.chakra.w300_light.clone(),
-                font_size: 25.0 * UI_SCALE,
-                color: colors::TRUCKUI_TEXT_COLOR,
-            },
-        )
-        .with_style(Style {
-            margin: UiRect::all(Val::Px(4.0 * UI_SCALE)),
-            ..default()
-        }),
-    );
-    p.spawn(NodeBundle {
-        style: Style { ..default() },
-        ..default()
-    })
-    .with_children(|p| {
-        p.spawn(MaterialNodeBundle {
-            material: materials.add(UIPanelMaterial {
-                color: colors::TRUCKUI_BGCOLOR,
-            }),
+        );
+        p.spawn(NodeBundle {
             style: Style {
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                display: Display::Grid,
-                grid_template_columns: RepeatedGridTrack::flex(6, 1.0),
-                grid_template_rows: RepeatedGridTrack::flex(3, 1.0),
-                grid_auto_flow: GridAutoFlow::Row,
-                row_gap: Val::Px(6.0 * UI_SCALE),
-                column_gap: Val::Px(6.0 * UI_SCALE),
-                min_height: Val::Px(200.0 * UI_SCALE),
-                max_width: Val::Px(600.0 * UI_SCALE),
-                padding: UiRect::all(Val::Px(12.0 * UI_SCALE)),
-                margin: UiRect::all(Val::Px(2.0 * UI_SCALE)),
+                justify_content: JustifyContent::FlexStart,
+                flex_direction: FlexDirection::Row,
+                flex_grow: 0.04,
                 ..default()
             },
             ..default()
         })
         .with_children(|p| {
-            let tg = truckgear::TruckGear::new();
-            for gear in &tg.inventory {
+            p.spawn(equipment_frame(materials)).with_children(|p| {
                 p.spawn(button())
-                    .insert(LoadoutButton::Van(gear.clone()))
+                    .insert(LoadoutButton::Inventory(playergear::Inventory::new_left()))
                     .with_children(|p| {
-                        p.spawn(equipment(gear.get_sprite_idx()));
+                        p.spawn(equipment_def())
+                            .insert(playergear::Inventory::new_left());
                     });
-            }
+            });
+            p.spawn(equipment_frame(materials)).with_children(|p| {
+                p.spawn(button())
+                    .insert(LoadoutButton::Inventory(playergear::Inventory::new_right()))
+                    .with_children(|p| {
+                        p.spawn(equipment_def())
+                            .insert(playergear::Inventory::new_right());
+                    });
+            });
+            p.spawn(equipment_frame(materials)).with_children(|p| {
+                for i in 0..2 {
+                    p.spawn(button())
+                        .insert(LoadoutButton::InventoryNext(
+                            playergear::InventoryNext::new(i),
+                        ))
+                        .with_children(|p| {
+                            p.spawn(equipment_def())
+                                .insert(playergear::InventoryNext::new(i));
+                        });
+                }
+            });
+        });
+        p.spawn(
+            TextBundle::from_section(
+                "Van Inventory:",
+                TextStyle {
+                    font: handles.fonts.chakra.w300_light.clone(),
+                    font_size: 25.0 * UI_SCALE,
+                    color: colors::TRUCKUI_TEXT_COLOR,
+                },
+            )
+            .with_style(Style {
+                margin: UiRect::all(Val::Px(4.0 * UI_SCALE)),
+                ..default()
+            }),
+        );
+        p.spawn(NodeBundle {
+            style: Style { ..default() },
+            ..default()
+        })
+        .with_children(|p| {
+            p.spawn(MaterialNodeBundle {
+                material: materials.add(UIPanelMaterial {
+                    color: colors::TRUCKUI_BGCOLOR,
+                }),
+                style: Style {
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    display: Display::Grid,
+                    grid_template_columns: RepeatedGridTrack::flex(5, 1.0),
+                    grid_template_rows: RepeatedGridTrack::flex(5, 1.0),
+                    grid_auto_flow: GridAutoFlow::Row,
+                    row_gap: Val::Px(6.0 * UI_SCALE),
+                    column_gap: Val::Px(6.0 * UI_SCALE),
+                    min_height: Val::Px(200.0 * UI_SCALE),
+                    max_width: Val::Px(600.0 * UI_SCALE),
+                    padding: UiRect::all(Val::Px(12.0 * UI_SCALE)),
+                    margin: UiRect::all(Val::Px(2.0 * UI_SCALE)),
+                    ..default()
+                },
+                ..default()
+            })
+            .with_children(|p| {
+                let tg = truckgear::TruckGear::new();
+                for gear in &tg.inventory {
+                    p.spawn(button())
+                        .insert(LoadoutButton::Van(gear.clone()))
+                        .with_children(|p| {
+                            p.spawn(equipment(gear.get_sprite_idx()));
+                        });
+                }
+            });
+        });
+    };
+    p.spawn(NodeBundle {
+        style: Style {
+            flex_direction: FlexDirection::Row,
+            ..default()
+        },
+        ..default()
+    })
+    .with_children(|p| {
+        p.spawn(NodeBundle {
+            style: Style {
+                flex_direction: FlexDirection::Column,
+                flex_basis: Val::Percent(60.0),
+                flex_grow: 1.0,
+                flex_shrink: 0.0,
+                min_width: Val::Px(350.0 * UI_SCALE),
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(left_side);
+        p.spawn(NodeBundle {
+            style: Style {
+                flex_direction: FlexDirection::Column,
+                flex_basis: Val::Percent(40.0),
+                flex_grow: 0.0,
+                flex_shrink: 1.0,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|p| {
+            // ---- Right side of the window ----
+            p.spawn(
+                TextBundle::from_section(
+                    "Help and Item description:",
+                    TextStyle {
+                        font: handles.fonts.chakra.w300_light.clone(),
+                        font_size: 25.0 * UI_SCALE,
+                        color: colors::TRUCKUI_TEXT_COLOR,
+                    },
+                )
+                .with_style(Style {
+                    margin: UiRect::all(Val::Px(4.0 * UI_SCALE)),
+                    ..default()
+                }),
+            );
+
+            p.spawn(
+                TextBundle::from_section(
+                    "Select ...",
+                    TextStyle {
+                        font: handles.fonts.titillium.w400_regular.clone(),
+                        font_size: 22.0 * UI_SCALE,
+                        color: colors::TRUCKUI_TEXT_COLOR.with_a(0.7),
+                    },
+                )
+                .with_style(Style {
+                    margin: UiRect::all(Val::Px(4.0 * UI_SCALE)),
+                    flex_grow: 0.0,
+                    flex_shrink: 0.0,
+                    flex_basis: Val::Px(400.0 * UI_SCALE),
+                    height: Val::Px(220.0 * UI_SCALE),
+                    overflow: Overflow::visible(),
+                    ..default()
+                }),
+            )
+            .insert(GearHelp);
         });
     });
 }
