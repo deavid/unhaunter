513756605208ba8b0af0c296172ea01dea1f4574
David Martinez Marti <deavidsedice@gmail.com>
2024-10-27 09:10:05 +0000

    further refactor manual

--- src/manual/chapter1/p01_mission_briefing.rs
+++ src/manual/chapter1/p01_mission_briefing.rs
@@ -1,105 +1,39 @@
-use super::super::utils::image_text;
-use crate::platform::plt::UI_SCALE;
+use crate::manual::utils::{grid_img_text, header};
+
 use crate::root::GameAssets;
 use bevy::prelude::*;
 
 pub fn draw_mission_briefing_page(parent: &mut ChildBuilder, handles: &GameAssets) {
-    // Mission Briefing Section
-    parent.spawn(NodeBundle {
-        style: Style {
-            // Occupy full width
-            width: Val::Percent(100.0),
-            height: Val::Percent(10.0),
-            justify_content: JustifyContent::Center,
-            align_items: AlignItems::Center,
-            flex_direction: FlexDirection::Column,
-            ..default()
-        },
-        ..default()
-    }).with_children(|parent| {
-        // Headline
-        parent.spawn(TextBundle::from_section("Paranormal Investigator Needed!", TextStyle {
-            font: handles.fonts.londrina.w300_light.clone(),
-            font_size: 48.0 * UI_SCALE,
-            color: Color::WHITE,
-        }));
-
-        // Premise Text
-        parent.spawn(
-            TextBundle::from_section(
-                "Reports of unsettling activity... restless spirits... your expertise is required to investigate and expel the ghosts haunting these locations.",
-                TextStyle {
-                    font: handles.fonts.chakra.w400_regular.clone(),
-                    font_size: 24.0 * UI_SCALE,
-                    color: Color::WHITE,
-                },
-            ),
-        );
-    });
-
-    // Gameplay Loop Section (3x2 Grid)
-    parent.spawn(NodeBundle {
-        style: Style {
-            // Occupy full width
-            width: Val::Percent(100.0),
-            height: Val::Percent(70.0),
-            display: Display::Grid,
-            // 3 equal-width columns
-            grid_template_columns: RepeatedGridTrack::flex(3, 1.0),
-            grid_template_rows: RepeatedGridTrack::percent(2, 60.0),
-            column_gap: Val::Percent(4.0),
-            row_gap: Val::Percent(4.0),
-            padding: UiRect::all(Val::Percent(2.0)),
-            ..default()
-        },
-        ..default()
-    }).with_children(|parent| {
-        // Step 1: Investigate
-        image_text(
-            parent,
+    let title = "Paranormal Investigator Needed!";
+    let subtitle = "Reports of unsettling activity... restless spirits... your expertise is required to investigate and expel the ghosts haunting these locations.";
+    let grid = vec![
+        (
             &handles.images.manual_investigate,
-            &handles.fonts.chakra.w400_regular,
-            "Explore the location, search for clues, and use your equipment to detect paranormal activity.",
-        );
-
-        // Step 2: Locate Ghost
-        image_text(
-            parent,
+            "Explore the location, search for clues, and use your equipment to detect paranormal activity."
+        ),
+        (
             &handles.images.manual_locate_ghost,
-            &handles.fonts.chakra.w400_regular,
-            "Find the ghost's breach, a subtle dust cloud that marks its presence.",
-        );
-
-        // Step 3: Identify Ghost
-        image_text(
-            parent,
+          "Find the ghost's breach, a subtle dust cloud that marks its presence.",
+        ),
+        (
             &handles.images.manual_identify_ghost,
-            &handles.fonts.chakra.w400_regular,
             "Gather evidence using your equipment and record your findings in the truck journal to identify the ghost type.",
-        );
-
-        // Step 4: Craft Repellent
-        image_text(
-            parent,
+        ),
+        (
             &handles.images.manual_craft_repellent,
-            &handles.fonts.chakra.w400_regular,
             "Once you've identified the ghost, craft a specialized repellent in the truck.",
-        );
-
-        // Step 5: Expel Ghost
-        image_text(
-            parent,
+        ),
+        (
             &handles.images.manual_expel_ghost,
-            &handles.fonts.chakra.w400_regular,
             "Confront the ghost and use the repellent to banish it from the location.",
-        );
-
-        // Step 6: End Mission
-        image_text(
-            parent,
+        ),
+        (
             &handles.images.manual_end_mission,
-            &handles.fonts.chakra.w400_regular,
             "Return to the truck and click 'End Mission' to complete the investigation and receive your score.",
-        );
-    });
+        ),
+    ];
+
+    header(parent, handles, title, subtitle);
+
+    grid_img_text(parent, &handles.fonts.chakra.w400_regular, (3, 2), grid);
 }
--- src/manual/utils.rs
+++ src/manual/utils.rs
@@ -1,4 +1,6 @@
 use crate::platform::plt::UI_SCALE;
+use crate::root::GameAssets;
+
 use bevy::prelude::*;
 
 pub fn image_text(
@@ -34,6 +36,78 @@ pub fn image_text(
                 TextStyle {
                     font: font.clone(),
                     font_size: 18.0 * UI_SCALE,
+                    color: Color::WHITE,
+                },
+            ));
+        });
+}
+
+pub fn grid_img_text(
+    parent: &mut ChildBuilder<'_>,
+    font: &Handle<Font>,
+    colxrow: (u16, u16),
+    contents: Vec<(&Handle<Image>, impl Into<String>)>,
+) {
+    // Gameplay Loop Section (3x2 Grid)
+    parent
+        .spawn(NodeBundle {
+            style: Style {
+                // Occupy full width
+                width: Val::Percent(100.0),
+                height: Val::Percent(70.0),
+                display: Display::Grid,
+                grid_template_columns: RepeatedGridTrack::flex(colxrow.0, 1.0),
+                grid_template_rows: RepeatedGridTrack::percent(colxrow.1, 60.0),
+                column_gap: Val::Percent(4.0),
+                row_gap: Val::Percent(4.0),
+                padding: UiRect::all(Val::Percent(2.0)),
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            for (img, text) in contents {
+                image_text(parent, img, font, text);
+            }
+        });
+}
+
+pub fn header(
+    parent: &mut ChildBuilder,
+    handles: &GameAssets,
+    title: impl Into<String>,
+    subtitle: impl Into<String>,
+) {
+    parent
+        .spawn(NodeBundle {
+            style: Style {
+                // Occupy full width
+                width: Val::Percent(100.0),
+                height: Val::Percent(10.0),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                flex_direction: FlexDirection::Column,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            // Headline
+            parent.spawn(TextBundle::from_section(
+                title,
+                TextStyle {
+                    font: handles.fonts.londrina.w300_light.clone(),
+                    font_size: 48.0 * UI_SCALE,
+                    color: Color::WHITE,
+                },
+            ));
+
+            // Premise Text
+            parent.spawn(TextBundle::from_section(
+                subtitle,
+                TextStyle {
+                    font: handles.fonts.chakra.w400_regular.clone(),
+                    font_size: 24.0 * UI_SCALE,
                     color: Color::WHITE,
                 },
             ));
