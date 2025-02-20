e4784561c626c0fdfc890d84a618a593d1faa745
David Martinez Marti <deavidsedice@gmail.com>
2025-02-20 20:45:54 +0000

    Added KELVIN_OFFSET and conversion functions

--- uncore/src/lib.rs
+++ uncore/src/lib.rs
@@ -16,3 +16,18 @@ pub mod utils;
 
 /// Enables/disables debug logs related to the player.
 pub const DEBUG_PLAYER: bool = false;
+
+/// Zero degrees Celsius in Kelvin.
+pub const KELVIN_OFFSET: f32 = 273.15;
+
+/// Converts a temperature from Celsius to Kelvin.
+#[inline]
+pub fn celsius_to_kelvin(celsius: f32) -> f32 {
+    celsius + KELVIN_OFFSET
+}
+
+/// Converts a temperature from Kelvin to Celsius.
+#[inline]
+pub fn kelvin_to_celsius(kelvin: f32) -> f32 {
+    kelvin - KELVIN_OFFSET
+}
