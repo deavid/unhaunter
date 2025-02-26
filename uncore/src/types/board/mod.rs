f4e4064282c72f1ce1c28fe94b28648618f5ca19
David Martinez Marti <deavidsedice@gmail.com>
2025-02-26 20:54:32 +0000

    Replace custom LightFieldSector implementation with ndarray::Array3 for storing and calculating light data.

--- uncore/src/types/board/mod.rs
+++ uncore/src/types/board/mod.rs
@@ -1,4 +1,3 @@
 pub mod cached_board_pos;
 pub mod fielddata;
 pub mod light;
-pub mod light_field_sector;
--- unstd/src/plugins/board.rs
+++ unstd/src/plugins/board.rs
@@ -2,11 +2,14 @@
 //! It includes systems for applying isometric perspective, rebuilding collision data, and updating
 //! the lighting field based on the current state of the board and behaviors.
 
+use std::time::Duration;
+
 use bevy::diagnostic::{Diagnostic, DiagnosticPath, RegisterDiagnostic};
 use bevy::prelude::*;
 use bevy::utils::Instant;
 use fastapprox::faster;
 
+use ndarray::Array3;
 use uncore::behavior::Behavior;
 use uncore::components::board::boardposition::BoardPosition;
 use uncore::components::board::position::Position;
@@ -15,7 +18,6 @@ use uncore::metric_recorder::SendMetric;
 use uncore::resources::roomdb::RoomDB;
 use uncore::resources::visibility_data::VisibilityData;
 use uncore::types::board::cached_board_pos::CachedBoardPos;
-use uncore::types::board::light_field_sector::LightFieldSector;
 use uncore::{
     resources::board_data::BoardData,
     types::board::fielddata::{CollisionFieldData, LightFieldData},
@@ -151,213 +153,184 @@ pub fn rebuild_lighting_field(bf: &mut BoardData, qt: &Query<(&Position, &Behavi
     let build_start_time = Instant::now();
     let cbp = CachedBoardPos::new();
     bf.exposure_lux = 1.0;
-    bf.light_field.fill(LightFieldData::default());
+    if bf.light_field.dim() != bf.map_size {
+        return;
+    }
+    let mut lfs = Array3::from_elem(bf.map_size, LightFieldData::default());
 
-    // Dividing by 4 so later we don't get an overflow if there's no map.
-    let first_p = qt
-        .iter()
-        .next()
-        .map(|(p, _b)| p.to_board_position())
-        .unwrap_or_default();
-    let mut min_x = first_p.x;
-    let mut min_y = first_p.y;
-    let mut min_z = first_p.z;
-    let mut max_x = first_p.x;
-    let mut max_y = first_p.y;
-    let mut max_z = first_p.z;
+    let def_light = LightFieldData::default();
     for (pos, behavior) in qt.iter() {
         let pos = pos.to_board_position();
-        min_x = min_x.min(pos.x);
-        min_y = min_y.min(pos.y);
-        min_z = min_z.min(pos.z);
-        max_x = max_x.max(pos.x);
-        max_y = max_y.max(pos.y);
-        max_z = max_z.max(pos.z);
-        let src = &bf.light_field[pos.ndidx()];
-        bf.light_field[pos.ndidx()] = LightFieldData {
-            lux: behavior.p.light.emmisivity_lumens() + src.lux,
-            transmissivity: behavior.p.light.transmissivity_factor() * src.transmissivity + 0.0001,
-            additional: src.additional.add(&behavior.p.light.additional_data()),
+        lfs[pos.ndidx()] = LightFieldData {
+            lux: behavior.p.light.emmisivity_lumens() + def_light.lux,
+            transmissivity: behavior.p.light.transmissivity_factor() * def_light.transmissivity
+                + 0.0001,
+            additional: def_light
+                .additional
+                .add(&behavior.p.light.additional_data()),
         };
     }
+    warn!("Map Size: {:?}", bf.map_size);
 
-    // info!( "Collecting time: {:?} - sz: {}", build_start_time.elapsed(),
-    // bf.light_field.len() );
-    let mut lfs = LightFieldSector::new(min_x, min_y, min_z, max_x, max_y, max_z);
-    for (k, v) in bf.light_field.indexed_iter() {
-        lfs.insert(k.0 as i64, k.1 as i64, k.2 as i64, v.clone());
-    }
-
-    // let mut lfs_clone_time_total = Duration::ZERO; let mut shadows_time_total =
-    // Duration::ZERO; let mut store_lfs_time_total = Duration::ZERO;
     for step in 0..3 {
-        // let lfs_clone_time = Instant::now();
         let src_lfs = lfs.clone();
 
         // lfs_clone_time_total += lfs_clone_time.elapsed();
         let size = match step {
             0 => 26,
-            1 => 8,
-            2 => 6,
+            1 => 6,
+            2 => 3,
             3 => 3,
             _ => 6,
         };
-        for x in min_x..=max_x {
-            for y in min_y..=max_y {
-                for z in min_z..=max_z {
-                    let Some(src) = src_lfs.get(x, y, z) else {
-                        continue;
-                    };
+        let mut total_time1 = Duration::default();
+        let mut total_time2 = Duration::default();
 
-                    // if src.transmissivity < 0.5 && step > 0 && size > 1 { // Reduce light spread
-                    // through walls // FIXME: If the light is on the wall, this breaks (and this is
-                    // possible since the wall is really 1/3rd of the tile) continue; }
-                    let root_pos = BoardPosition { x, y, z };
-                    let mut src_lux = src.lux;
-                    let min_lux = match step {
-                        0 => 0.001,
-                        1 => 0.000001,
-                        _ => 0.0000000001,
-                    };
-                    let max_lux = match step {
-                        0 => f32::MAX,
-                        1 => 10000.0,
-                        2 => 1000.0,
-                        3 => 0.1,
-                        _ => 0.01,
-                    };
-                    if src_lux < min_lux {
-                        continue;
-                    }
-                    if src_lux > max_lux {
-                        continue;
-                    }
+        for (root_ndidx, src) in src_lfs.indexed_iter() {
+            let root_pos = BoardPosition {
+                x: root_ndidx.0 as i64,
+                y: root_ndidx.1 as i64,
+                z: root_ndidx.2 as i64,
+            };
+            let mut src_lux = src.lux;
+            let min_lux = match step {
+                0 => 0.001,
+                1 => 0.000001,
+                _ => 0.0000000001,
+            };
+            let max_lux = match step {
+                0 => f32::MAX,
+                1 => 10000.0,
+                2 => 1000.0,
+                3 => 0.1,
+                _ => 0.01,
+            };
+            if src_lux < min_lux {
+                continue;
+            }
+            if src_lux > max_lux {
+                continue;
+            }
 
-                    // Optimize next steps by only looking to harsh differences.
-                    let nbors =
-                        root_pos.iter_xy_neighbors_clamped(1, (min_x, min_y), (max_x, max_y));
+            if step > 0 {
+                // Optimize next steps by only looking to harsh differences.
+                let nbors = root_pos.iter_xy_neighbors(1, bf.map_size);
+                let ldata_iter = nbors.map(|b| {
+                    let l = &lfs[b.ndidx()];
+                    (
+                        ordered_float::OrderedFloat(l.lux),
+                        ordered_float::OrderedFloat(l.transmissivity),
+                    )
+                });
+                let mut min_lux = ordered_float::OrderedFloat(f32::MAX);
+                let mut max_lux = ordered_float::OrderedFloat(0.0);
+                let mut min_trans = ordered_float::OrderedFloat(2.0);
+                for (lux, trans) in ldata_iter {
+                    min_lux = min_lux.min(lux);
+                    max_lux = max_lux.max(lux);
+                    min_trans = min_trans.min(trans);
+                }
 
-                    if step > 0 {
-                        let ldata_iter = nbors.filter_map(|b| {
-                            lfs.get_pos(&b).map(|l| {
-                                (
-                                    ordered_float::OrderedFloat(l.lux),
-                                    ordered_float::OrderedFloat(l.transmissivity),
-                                )
-                            })
-                        });
-                        let mut min_lux = ordered_float::OrderedFloat(f32::MAX);
-                        let mut min_trans = ordered_float::OrderedFloat(2.0);
-                        for (lux, trans) in ldata_iter {
-                            min_lux = min_lux.min(lux);
-                            min_trans = min_trans.min(trans);
-                        }
-
-                        // For smoothing steps only:
-                        if *min_trans > 0.7 && src_lux / (*min_lux + 0.0001) < 1.9 {
-                            // If there are no walls nearby, we don't reflect light.
-                            continue;
-                        }
-                    }
-
-                    // This controls how harsh is the light
-                    if step > 0 {
-                        src_lux /= 5.5;
-                    } else {
-                        src_lux /= 1.01;
-                    }
-
-                    // let shadows_time = Instant::now(); This takes time to process:
-                    let nbors =
-                        root_pos.iter_xy_neighbors_clamped(size, (min_x, min_y), (max_x, max_y));
-
-                    // reset the light value for this light, so we don't count double.
-                    lfs.get_mut_pos(&root_pos).unwrap().lux -= src_lux;
-                    let mut shadow_dist = [(size + 1) as f32; CachedBoardPos::TAU_I];
-
-                    // Compute shadows
-                    for pillar_pos in nbors.clone() {
-                        // 60% of the time spent in compute shadows is obtaining this:
-                        let Some(lf) = lfs.get_pos(&pillar_pos) else {
-                            continue;
-                        };
-
-                        // let lf = unsafe { lfs.get_pos_unchecked(pillar_pos) }; t_x += lf.lux; continue;
-                        let consider_opaque = lf.transmissivity < 0.5;
-                        if !consider_opaque {
-                            continue;
-                        }
-                        let min_dist = cbp.bpos_dist(&root_pos, &pillar_pos);
-                        let angle = cbp.bpos_angle(&root_pos, &pillar_pos);
-                        let angle_range = cbp.bpos_angle_range(&root_pos, &pillar_pos);
-                        for d in angle_range.0..=angle_range.1 {
-                            let ang = (angle as i64 + d).rem_euclid(CachedBoardPos::TAU_I as i64)
-                                as usize;
-                            shadow_dist[ang] = shadow_dist[ang].min(min_dist);
-                        }
-                    }
-
-                    // shadows_time_total += shadows_time.elapsed(); FIXME: Possibly we want to smooth
-                    // shadow_dist here - a convolution with a gaussian or similar where we preserve
-                    // the high values but smooth the transition to low ones.
-                    if src.transmissivity < 0.5 {
-                        // Reduce light spread through walls
-                        shadow_dist.iter_mut().for_each(|x| *x = 0.0);
-                    }
-
-                    // let size = shadow_dist .iter() .map(|d| (d + 1.5).round() as u32) .max()
-                    // .unwrap() .min(size); let nbors = root_pos.xy_neighbors(size);
-                    let light_height = 4.0;
-
-                    // let mut total_lux = 0.1; for neighbor in nbors.iter() { let dist =
-                    // cbp.bpos_dist(&root_pos, neighbor); let dist2 = dist + light_height; let angle
-                    // = cbp.bpos_angle(&root_pos, neighbor); let sd = shadow_dist[angle]; let f =
-                    // (faster::tanh(sd - dist - 0.5) + 1.0) / 2.0; total_lux += f / dist2 / dist2; }
-                    // let store_lfs_time = Instant::now();
-                    let total_lux = 2.0;
-
-                    // new shadow method
-                    for neighbor in nbors {
-                        let dist = cbp.bpos_dist(&root_pos, &neighbor);
-
-                        // let dist = root_pos.fast_distance_xy(neighbor);
-                        let dist2 = dist + light_height;
-                        let angle = cbp.bpos_angle(&root_pos, &neighbor);
-                        let sd = shadow_dist[angle];
-                        let lux_add = src_lux / dist2 / dist2 / total_lux;
-                        if dist - 3.0 < sd {
-                            // FIXME: f here controls the bleed through walls.
-                            if let Some(lf) = lfs.get_mut_pos(&neighbor) {
-                                // 0.5 is too low, it creates un-evenness.
-                                const BLEED_TILES: f32 = 0.8;
-                                let f = (faster::tanh((sd - dist - 0.5) * BLEED_TILES.recip())
-                                    + 1.0)
-                                    / 2.0;
-
-                                // let f = 1.0;
-                                lf.lux += lux_add * f;
-                            }
-                        }
-                    }
-                    // store_lfs_time_total += store_lfs_time.elapsed();
+                // For smoothing steps only:
+                if *max_lux / (*min_lux + 0.0001) < 1.2 {
+                    continue;
+                }
+                if *min_trans > 0.7 && src_lux / (*min_lux + 0.0001) < 1.9 {
+                    // If there are no walls nearby, we don't reflect light.
+                    continue;
                 }
             }
+
+            // This controls how harsh is the light
+            if step > 0 {
+                src_lux /= 5.5;
+            } else {
+                src_lux /= 1.01;
+            }
+
+            // let shadows_time = Instant::now(); This takes time to process:
+            let nbors = root_pos.iter_xy_neighbors(size, bf.map_size);
+
+            // reset the light value for this light, so we don't count double.
+            lfs[root_ndidx].lux -= src_lux;
+            let mut shadow_dist = [(size + 1) as f32; CachedBoardPos::TAU_I];
+
+            let time1 = bevy::utils::Instant::now();
+
+            // Compute shadows
+            for pillar_pos in nbors.clone() {
+                // 60% of the time spent in compute shadows is obtaining this:
+                let lf = &lfs[pillar_pos.ndidx()];
+
+                // let lf = unsafe { lfs.get_pos_unchecked(pillar_pos) }; t_x += lf.lux; continue;
+                let consider_opaque = lf.transmissivity < 0.5;
+                if !consider_opaque {
+                    continue;
+                }
+                let min_dist = cbp.bpos_dist(&root_pos, &pillar_pos);
+                let angle = cbp.bpos_angle(&root_pos, &pillar_pos);
+                let angle_range = cbp.bpos_angle_range(&root_pos, &pillar_pos);
+                for d in angle_range.0..=angle_range.1 {
+                    let ang = (angle as i64 + d).rem_euclid(CachedBoardPos::TAU_I as i64) as usize;
+                    shadow_dist[ang] = shadow_dist[ang].min(min_dist);
+                }
+            }
+            total_time1 += time1.elapsed();
+
+            // shadows_time_total += shadows_time.elapsed(); FIXME: Possibly we want to smooth
+            // shadow_dist here - a convolution with a gaussian or similar where we preserve
+            // the high values but smooth the transition to low ones.
+            if src.transmissivity < 0.5 {
+                // Reduce light spread through walls
+                shadow_dist.iter_mut().for_each(|x| *x = 0.0);
+            }
+
+            // let size = shadow_dist .iter() .map(|d| (d + 1.5).round() as u32) .max()
+            // .unwrap() .min(size); let nbors = root_pos.xy_neighbors(size);
+            let light_height = 4.0;
+
+            // let mut total_lux = 0.1; for neighbor in nbors.iter() { let dist =
+            // cbp.bpos_dist(&root_pos, neighbor); let dist2 = dist + light_height; let angle
+            // = cbp.bpos_angle(&root_pos, neighbor); let sd = shadow_dist[angle]; let f =
+            // (faster::tanh(sd - dist - 0.5) + 1.0) / 2.0; total_lux += f / dist2 / dist2; }
+            // let store_lfs_time = Instant::now();
+            let total_lux = 2.0;
+            let time2 = bevy::utils::Instant::now();
+
+            // new shadow method
+            for neighbor in nbors {
+                let dist = cbp.bpos_dist(&root_pos, &neighbor);
+
+                // let dist = root_pos.fast_distance_xy(neighbor);
+                let dist2 = dist + light_height;
+                let angle = cbp.bpos_angle(&root_pos, &neighbor);
+                let sd = shadow_dist[angle];
+                let lux_add = src_lux / dist2 / dist2 / total_lux;
+                if dist - 3.0 < sd {
+                    // FIXME: f here controls the bleed through walls.
+                    // 0.5 is too low, it creates un-evenness.
+                    const BLEED_TILES: f32 = 0.8;
+                    let f = (faster::tanh((sd - dist - 0.5) * BLEED_TILES.recip()) + 1.0) / 2.0;
+
+                    // let f = 1.0;
+                    lfs[neighbor.ndidx()].lux += lux_add * f;
+                }
+            }
+            total_time2 += time2.elapsed();
+            // store_lfs_time_total += store_lfs_time.elapsed();
         }
+        warn!("Time to compute shadows: {step} - {:?}", total_time1);
+        warn!("Time to store lfs: {step} - {:?}", total_time2);
         // info!( "Light step {}: {:?}; per size: {:?}", step, step_time.elapsed(),
         // step_time.elapsed() / size );
     }
-    for (k, v) in bf.light_field.indexed_iter_mut() {
-        let bpos = BoardPosition::from_ndidx(k);
-        v.lux = lfs.get_pos(&bpos).unwrap().lux;
-    }
 
     // let's get an average of lux values
-    let mut total_lux = 0.0;
-    for v in bf.light_field.iter() {
-        total_lux += v.lux;
-    }
-    let avg_lux = total_lux / bf.light_field.len() as f32;
+    let total_lux: f32 = lfs.iter().map(|x| x.lux).sum();
+    let count = (bf.map_size.0 * bf.map_size.1 * bf.map_size.2) as f32;
+    let avg_lux = total_lux / count;
     bf.exposure_lux = (avg_lux + 2.0) / 2.0;
+    bf.light_field = lfs;
 
     // dbg!(lfs_clone_time_total); dbg!(shadows_time_total);
     // dbg!(store_lfs_time_total);
