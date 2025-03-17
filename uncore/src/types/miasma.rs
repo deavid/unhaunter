cff6d8cdb2e5e1c1eaf43d6430c7cb98416033b5
David Martinez Marti <deavidsedice@gmail.com>
2025-03-17 15:57:19 +0000

    Revert "test workflow failure"
    This reverts commit ba7771007142053f88d2cc9ac9e906c5f8c85b3a.

--- uncore/src/types/miasma.rs
+++ uncore/src/types/miasma.rs
@@ -3,7 +3,7 @@ use ndarray::Array3;
 
 #[derive(Debug, Clone, Default)]
 pub struct MiasmaGrid {
-    pub pressure_field: Array3<f32 test>,
+    pub pressure_field: Array3<f32>,
     pub velocity_field: Array3<Vec2>,
     pub room_modifiers: HashMap<String, f32>, // Room ID -> Modifier
 }
