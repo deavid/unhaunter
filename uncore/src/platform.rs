04e3b1b2ba900607a4754a42fa9524c64f057313
David Martínez Martí <deavidsedice@gmail.com>
2025-12-08 19:32:22 +0000

    fix: update UI_SCALE for wasm target

--- uncore/src/platform.rs
+++ uncore/src/platform.rs
@@ -10,7 +10,7 @@ pub mod plt {
 #[cfg(target_arch = "wasm32")]
 pub mod plt {
     pub const IS_WASM: bool = true;
-    pub const UI_SCALE: f32 = 0.8;
+    pub const UI_SCALE: f32 = 1.0;
     pub const FONT_SCALE: f32 = UI_SCALE / 1.2;
     pub const ASPECT_RATIO: f32 = 16.0 / 10.0;
     pub static VERSION: &str = env!("CARGO_PKG_VERSION");
